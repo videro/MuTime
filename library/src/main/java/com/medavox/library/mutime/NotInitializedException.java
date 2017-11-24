c5c4b3fa5f0656bf10d7570f95ca172f0334f2e8
Dominik Louven <dlouven@videro.com>
2017-11-24 11:09:54 +0100

    Added new Exception class

---
+++ library/src/main/java/com/medavox/library/mutime/NotInitializedException.java
@@ -0,0 +1,11 @@
+package com.medavox.library.mutime;
+
+/**
+ * Created by dlouven on 24.11.17.
+ */
+
+public class NotInitializedException extends Exception{
+    public NotInitializedException(String msg){
+        super(msg);
+    }
+}
